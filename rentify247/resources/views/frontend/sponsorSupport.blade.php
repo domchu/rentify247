b73be97f55201ba64cd1c2a6def8119c1a46e49e
Mr.Dominic <ochu.dominic.onah@gmail.com>
2024-12-11 23:02:29 +0100

    chore: ft-sponsor interface

--- rentify247/resources/views/frontend/sponsorSupport.blade.php
+++ rentify247/resources/views/frontend/sponsorSupport.blade.php
@@ -1,21 +1,38 @@
 <div class="bg-sky-900 w-full h-screen">
-
-    <div class="text-center my-8 mx-auto h-[32rem]">
+    {{-- sponsor title --}}
+    <div class="text-center  mx-auto h-[15rem]">
         <h2 class="text-white">Support Rentify247 utility</h2>
         <p class="w-[32rem] my-0 mx-auto text-white">Our maintainers devote their time, effort, and heart to ensure Chakra UI keeps getting better. Support us by donating to our collective 🙏</p>
     </div>
 
-
-  <div class="collective">
-    <div class="bg-white flex">
-        <div class="flex">
-            <img src="/assets/sponsor.png" alt="Sponsor Image">
-            <div class="text">
-                <h3 class="text-slate-900">Open Collective</h3>
-                <p>Sponsor the Rentify247 maintainers & Storage</p>
+    {{-- collective sponsor --}}
+    <div class="collective">
+        <div class="bg-white flex justify-between items-center py-[20px]  mx-auto rounded-md w-3/6 px-5">
+            <div class="flex justify-between items-center my-2 mx-4">
+                <img src="/assets/Sponsor-icon.png" alt="Sponsor Image" class="w-[50px] h-[50px] rounded-md">
+                <div class="ml-6">
+                    <h3 class="text-slate-900 font-bold text-xl">Open Collective</h3>
+                    <p class="my-1 text-gray-500 font-semibold">Sponsor the Rentify247 maintainers & Storage</p>
+                </div>
+            </div>
+            <div class="link">
+                <a href="/" class="py-3 px-6 bg-sky-800 text-white rounded-md">Sponsor</a>
             </div>
         </div>
-        <a href="/">Sponsor</a>
     </div>
-</div>
+    {{-- created sponsor --}}
+    <div class="collective mt-10">
+        <div class="bg-white flex justify-between items-center py-[20px]  mx-auto rounded-md w-3/6 px-5">
+            <div class="flex justify-between items-center my-2 mx-4">
+                <img src="/assets/Sponsor-icon.png" alt="Sponsor Image" class="w-[50px] h-[50px] rounded-md">
+                <div class="ml-6">
+                    <h3 class="text-slate-900 font-bold text-xl">Software Developer</h3>
+                    <p class="my-1 text-gray-500 font-semibold">Sponsor the creator</p>
+                </div>
+            </div>
+            <div class="link">
+                <a href="/" class="py-3 px-6 bg-sky-800 text-white rounded-md">Sponsor</a>
+            </div>
+        </div>
+    </div>
     <div>
