3679f0a7832e44d9dd8b1b25b8f1476e7098fe6f
Mr.Dominic <ochu.dominic.onah@gmail.com>
2024-12-12 17:18:57 +0100

    Chore: created the organisational support

--- rentify247/resources/views/frontend/sponsorSupport.blade.php
+++ rentify247/resources/views/frontend/sponsorSupport.blade.php
@@ -1,38 +1,74 @@
-<div class="bg-sky-900 w-full h-screen">
-    {{-- sponsor title --}}
-    <div class="text-center  mx-auto h-[15rem]">
-        <h2 class="text-white">Support Rentify247 utility</h2>
-        <p class="w-[32rem] my-0 mx-auto text-white">Our maintainers devote their time, effort, and heart to ensure Chakra UI keeps getting better. Support us by donating to our collective 🙏</p>
-    </div>
+<div class="w-full bg-sky-900 lg:w-full h-screen py-4 relative">
+        {{-- sponsor title --}}
+        <div class="text-center mx-auto h-[15rem]">
+            <h2 class="text-white pt-8 text-5xl mb-3">Support Rentify247 utility</h2>
+            <p class="w-[32rem] my-2 mx-auto text-gray-300 text-[18px]">Our Software Developers/Team devote their time,
+                effort, heart and sleepless night to ensure Rentify247 becomes a dream come true. Support us by donating to
+                our collective 🙏</p>
+        </div>
 
-    {{-- collective sponsor --}}
-    <div class="collective">
-        <div class="bg-white flex justify-between items-center py-[20px]  mx-auto rounded-md w-3/6 px-5">
-            <div class="flex justify-between items-center my-2 mx-4">
-                <img src="/assets/Sponsor-icon.png" alt="Sponsor Image" class="w-[50px] h-[50px] rounded-md">
-                <div class="ml-6">
-                    <h3 class="text-slate-900 font-bold text-xl">Open Collective</h3>
-                    <p class="my-1 text-gray-500 font-semibold">Sponsor the Rentify247 maintainers & Storage</p>
+        {{-- collective sponsor --}}
+        <div class="collective">
+            <div class="md:w-5/6  bg-white flex justify-between items-center py-[20px] mx-auto rounded-md lg:w-3/6 px-5">
+                <div class="md:flex justify-between items-center my-2 mx-4">
+                    <img src="/assets/Sponsor-icon.png" alt="Sponsor Image" class="w-[50px] h-[50px] rounded-md">
+                    <div class="ml-6">
+                        <h3 class="text-slate-900 font-bold text-xl">Open Collective</h3>
+                        <p class="my-1 text-gray-500 font-semibold">Sponsor the Rentify247 maintainers & Storage</p>
+                    </div>
+                </div>
+                <div class="link">
+                    <a href="/" class="py-3 px-6 bg-sky-800 text-white rounded-md hover:bg-sky-700">Sponsor</a>
                 </div>
             </div>
-            <div class="link">
-                <a href="/" class="py-3 px-6 bg-sky-800 text-white rounded-md">Sponsor</a>
-            </div>
         </div>
-    </div>
-    {{-- created sponsor --}}
-    <div class="collective mt-10">
-        <div class="bg-white flex justify-between items-center py-[20px]  mx-auto rounded-md w-3/6 px-5">
-            <div class="flex justify-between items-center my-2 mx-4">
-                <img src="/assets/Sponsor-icon.png" alt="Sponsor Image" class="w-[50px] h-[50px] rounded-md">
-                <div class="ml-6">
-                    <h3 class="text-slate-900 font-bold text-xl">Software Developer</h3>
-                    <p class="my-1 text-gray-500 font-semibold">Sponsor the creator</p>
+        {{-- created sponsor --}}
+        <div class="collective mt-10">
+            <div class="md:w-5/6 bg-white flex justify-between items-center py-[20px] mx-auto rounded-md lg:w-3/6 px-5">
+                <div class="flex justify-between items-center my-2 mx-4">
+                    <img src="/assets/Sponsor-icon.png" alt="Sponsor Image" class="w-[50px] h-[50px] rounded-md">
+                    <div class="ml-6">
+                        <h3 class="text-slate-900 font-bold text-xl">Software Developer</h3>
+                        <p class="my-1 text-gray-500 font-semibold">Sponsor the creator</p>
+                    </div>
+                </div>
+                <div class="link">
+                    <a href="/" class="py-3 px-6 bg-sky-800 text-white rounded-md hover:bg-sky-700">Sponsor</a>
                 </div>
             </div>
-            <div class="link">
-                <a href="/" class="py-3 px-6 bg-sky-800 text-white rounded-md">Sponsor</a>
+        </div>
+        {{-- Organisational sponsor --}}
+        <div class="collective mt-12">
+            <h2 class="text-xl text-center font-bold text-[#fff] py-2">ORGANIZATION SPONSORS 🏦</h2>
+            <div class="company-images flex justify-center flex-wrap pt-4 col-6">
+                <div class="individual-image p-3">
+                    <img src="/assets/slack.jpg" alt="" sizes="" srcset="" class="w-[70px] h-[70px] rounded-full">
+                </div>
+                <div class="individual-image p-3">
+                    <img src="/assets/slack.jpg" alt="" sizes="" srcset="" class="w-[70px] h-[70px] rounded-full">
+                </div>
+                <div class="individual-image p-3">
+                    <img src="/assets/slack.jpg" alt="" sizes="" srcset="" class="w-[70px] h-[70px] rounded-full">
+                </div>
+                <div class="individual-image p-3">
+                    <img src="/assets/slack.jpg" alt="" sizes="" srcset="" class="w-[70px] h-[70px] rounded-full">
+                </div>
+                <div class="individual-image p-3">
+                    <img src="/assets/slack.jpg" alt="" sizes="" srcset="" class="w-[70px] h-[70px] rounded-full">
+                </div>
+                <div class="individual-image p-3">
+                    <img src="/assets/bttyg.jpg" alt="" sizes="" srcset="" class="w-[70px] h-[70px] rounded-full">
+                </div>
+                <div class="individual-image p-3">
+                    <img src="/assets/slack.jpg" alt="" sizes="" srcset="" class="w-[70px] h-[70px] rounded-full">
+                </div>
+                <div class="individual-image p-3">
+                    <img src="/assets/slack.jpg" alt="" sizes="" srcset="" class="w-[70px] h-[70px] rounded-full">
+                </div>
+                <div class="individual-image p-3">
+                    <img src="/assets/98173.jpg" alt="" sizes="" srcset="" class="w-[70px] h-[70px] rounded-full">
+                </div>
             </div>
         </div>
-    </div>
+        
     <div>
