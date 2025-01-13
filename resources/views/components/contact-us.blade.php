859f4ba5eee3526231eb55ea9a36aa3442909c0b
Mr.Dominic <ochu.dominic.onah@gmail.com>
2025-01-13 23:39:16 +0100

    chore: the structure of the contact page + title and text

--- resources/views/components/contact-us.blade.php
+++ resources/views/components/contact-us.blade.php
@@ -1,1 +1,39 @@
-<h1>contact us</h1>
+<section>
+    <div>
+        <div class="my-12">
+            <div class="contact-title max-w-full  bg-sky-800 text-center py-12">
+                <h2 class="text-white font-semibold text-3xl py-3 md:text-3xl lg:text-4xl">Contact Us</h2>
+                <p class="text-white px-7 pb-4 md:w-4/5 md:mx-auto md:my-0 lg:w-3/6 lg:mx-auto lg:my-0" ><span class="text-[#dc2d2d] font-bold text-xl px-1">Need an expert?</span> You are more than welcome to leave your contact <br>information and we will be in touch shortly</p>
+                    
+            </div>
+            <div class="sub-section">
+                <div class="left-content">
+
+                </div>
+                <div class="form-section">
+
+                </div>
+            </div>
+            {{-- ADDRESS SECTION --}}
+            <div class="address-section">
+                <div class="visit-us">
+
+                </div>
+                <div class="call-us">
+
+                </div>
+                <div class="message-us">
+
+                </div>
+            </div>
+            {{-- MAP --}}
+            <div class="map">
+
+            </div>
+            {{-- FOLLOW US --}}
+            <div class="social-media-icons">
+
+            </div>
+        </div>
+    </div>
+</section>
