778f88bf8c0f9c3fe943b4aa5cf74c9bda4bb0a8
Mr.Dominic <ochu.dominic.onah@gmail.com>
2025-01-14 01:35:41 +0100

    chore: style the form across all media screen

--- resources/views/components/contact-us.blade.php
+++ resources/views/components/contact-us.blade.php
@@ -9,40 +9,46 @@
 
             </div>
             <div
-                class="relative w-full h-84 mt-10 bg-[url('../../public/images/real-estate.webp')] bg-cover bg-center bg-no-repeat text-white p-10">
+                class="relative w-full h-84 mt-10 bg-[url('../../public/images/real-estate.webp')] bg-cover bg-center bg-no-repeat text-white ">
                 <div class="lg:flex lg:w-5/6 lg:mx-auto lg:my-0">
-                    <div class="lg:w-3/6 my-8">
+                    <div class="p-5 lg:w-3/6 my-8">
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis sunt perspiciatis mollitia
                             doloribus repellat adipisci dolorum voluptatum dicta velit, quae officiis fugit, possimus
                             inventore repellendus quaerat dolores architecto nihil aut?</p>
 
                     </div>
-                    <div class="form-section bg-sky-800 py-3 px-3 my-8 rounded lg:w-3/6">
+                    <div class="form-section bg-sky-800 py-3 px-3 w-full my-8 mx-auto  rounded lg:w-3/6">
                         <form action="" method="POST">
                             <div>
-                                <label for="" id="name" class="text-bold lg:mb-4">First Name<span class="text-[#d22929] font-bold ">*</span></label> <br>
+                                <label for="" id="name" class="text-bold lg:mb-4">First Name<span
+                                        class="text-[#d22929] font-bold ">*</span></label> <br>
                                 <input type="text" name="name" id="name" placeholder="First Name" required
-                                    autofocus="text-blue" class="rounded md:w-11/12 md:mt-2 lg:mt-2">
+                                    autofocus="text-blue" class="rounded text-black w-full lg:w-11/12 md:mt-2 lg:mt-2">
                             </div>
                             <div class="py-4">
-                                <label for="" id="first_name" class="text-bold">Last Name<span class="text-[#d22929] font-bold">*</span></label><br>
+                                <label for="" id="first_name" class="text-bold">Last Name<span
+                                        class="text-[#d22929] font-bold">*</span></label><br>
                                 <input type="text" name="last_name" id="name" placeholder="Last Name" required
-                                    autofocus="text-blue" class="rounded md:w-11/12 md:mt-2 lg:mt-2">
+                                    autofocus="text-blue" class="rounded text-black w-full lg:w-11/12 md:mt-2 lg:mt-2">
                             </div>
                             <div class="py-4">
-                                <label for="" id="email" class="text-bold">Enter Your Email<span class="text-[#d22929] font-bold">*</span></label><br>
-                                <input type="email" name="email" id="email" placeholder="example@gmail.com " required
-                                    autofocus="text-blue" class="rounded md:w-11/12 md:mt-2 lg:mt-2">
+                                <label for="" id="email" class="text-bold">Enter Your Email<span
+                                        class="text-[#d22929] font-bold">*</span></label><br>
+                                <input type="email" name="email" id="email" placeholder="example@gmail.com "
+                                    required autofocus="text-blue" class="rounded text-black w-full lg:w-11/12 md:mt-2 lg:mt-2">
                             </div>
                             <div class="py-4">
-                                <label for="" id="email" class="text-bold">Your Number<span class="text-[#d22929] font-bold">*</span></label><br>
-                                <input type="email" name="email" id="email" placeholder="(+234 1234567890) " required
-                                    autofocus="text-blue" class="rounded md:w-11/12 md:mt-2 lg:mt-2">
+                                <label for="" id="email" class="text-bold">Your Number<span
+                                        class="text-[#d22929] font-bold">*</span></label><br>
+                                <input type="email" name="email" id="email" placeholder="(+234 1234567890) "
+                                    required autofocus="text-blue" class="rounded text-black w-full lg:w-11/12 md:mt-2 lg:mt-2">
                             </div>
                             <div class="py-4">
-                                <label for="" id="email" class="text-bold">Your Message<span class="text-[#d22929] font-bold">*</span></label><br>
-                                <textarea name="message" id="message" cols="35" rows="10" placeholder="Write a Message" class="rounded md:w-11/12 md:mt-2 lg:mt-2" required></textarea>
-                               
+                                <label for="" id="email" class="text-bold">Your Message<span
+                                        class="text-[#d22929] font-bold">*</span></label><br>
+                                <textarea name="message" id="message" cols="35" rows="10" placeholder="Write a Message"
+                                    class="rounded text-black w-full lg:w-11/12 md:mt-2 lg:mt-2" required></textarea>
+
                             </div>
                         </form>
                     </div>
