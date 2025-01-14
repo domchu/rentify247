1ce61adbd93a0b1a88ea07e6a75fa034474006a1
Mr.Dominic <ochu.dominic.onah@gmail.com>
2025-01-14 01:03:06 +0100

    Chore: contact form added

--- resources/views/components/contact-us.blade.php
+++ resources/views/components/contact-us.blade.php
@@ -3,15 +3,49 @@
         <div class="my-12">
             <div class="contact-title max-w-full  bg-sky-800 text-center py-12">
                 <h2 class="text-white font-semibold text-3xl py-3 md:text-3xl lg:text-4xl">Contact Us</h2>
-                <p class="text-white px-7 pb-4 md:w-4/5 md:mx-auto md:my-0 lg:w-3/6 lg:mx-auto lg:my-0" ><span class="text-[#dc2d2d] font-bold text-xl px-1">Need an expert?</span> You are more than welcome to leave your contact <br>information and we will be in touch shortly</p>
-                    
+                <p class="text-white px-7 pb-4 text-wrap md:w-4/5 md:mx-auto md:my-0 lg:w-3/6 lg:mx-auto lg:my-0"><span
+                        class="text-[#dc2d2d] font-bold text-xl px-1">Need an expert?</span> You are more than welcome to
+                    leave your contact information and we will be in touch shortly.</p>
+
             </div>
-            <div class="sub-section">
-                <div class="left-content">
+            <div
+                class="relative w-full h-84 mt-10 bg-[url('../../public/images/real-estate.webp')] bg-cover bg-center bg-no-repeat text-white p-10">
+                <div class="lg:flex lg:w-5/6 lg:mx-auto lg:my-0">
+                    <div class="lg:w-3/6 my-8">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis sunt perspiciatis mollitia
+                            doloribus repellat adipisci dolorum voluptatum dicta velit, quae officiis fugit, possimus
+                            inventore repellendus quaerat dolores architecto nihil aut?</p>
 
-                </div>
-                <div class="form-section">
-
+                    </div>
+                    <div class="form-section bg-sky-800 py-3 px-3 my-8 rounded lg:w-3/6">
+                        <form action="" method="POST">
+                            <div>
+                                <label for="" id="name" class="text-bold lg:mb-4">First Name<span class="text-[#d22929] font-bold ">*</span></label> <br>
+                                <input type="text" name="name" id="name" placeholder="First Name" required
+                                    autofocus="text-blue" class="rounded md:w-11/12 md:mt-2 lg:mt-2">
+                            </div>
+                            <div class="py-4">
+                                <label for="" id="first_name" class="text-bold">Last Name<span class="text-[#d22929] font-bold">*</span></label><br>
+                                <input type="text" name="last_name" id="name" placeholder="Last Name" required
+                                    autofocus="text-blue" class="rounded md:w-11/12 md:mt-2 lg:mt-2">
+                            </div>
+                            <div class="py-4">
+                                <label for="" id="email" class="text-bold">Enter Your Email<span class="text-[#d22929] font-bold">*</span></label><br>
+                                <input type="email" name="email" id="email" placeholder="example@gmail.com " required
+                                    autofocus="text-blue" class="rounded md:w-11/12 md:mt-2 lg:mt-2">
+                            </div>
+                            <div class="py-4">
+                                <label for="" id="email" class="text-bold">Your Number<span class="text-[#d22929] font-bold">*</span></label><br>
+                                <input type="email" name="email" id="email" placeholder="(+234 1234567890) " required
+                                    autofocus="text-blue" class="rounded md:w-11/12 md:mt-2 lg:mt-2">
+                            </div>
+                            <div class="py-4">
+                                <label for="" id="email" class="text-bold">Your Message<span class="text-[#d22929] font-bold">*</span></label><br>
+                                <textarea name="message" id="message" cols="35" rows="10" placeholder="Write a Message" class="rounded md:w-11/12 md:mt-2 lg:mt-2" required></textarea>
+                               
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
             {{-- ADDRESS SECTION --}}
